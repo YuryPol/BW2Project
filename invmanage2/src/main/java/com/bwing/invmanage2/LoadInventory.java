b2b3dc57f9c691acef667c1ff8a529c91ea7861e
Yury Polyakovsky <Yury.Polyakovsky@disney.com>
2016-04-05 15:56:17 -0700

    started with loading inventory

--- invmanage2/src/main/java/com/bwing/invmanage2/LoadInventory.java
+++ invmanage2/src/main/java/com/bwing/invmanage2/LoadInventory.java
@@ -22,30 +22,30 @@ import com.google.appengine.tools.cloudstorage.RetryParams;
 public class LoadInventory extends HttpServlet 
 {
 	// Read file from the bucket and create tables in DB
-	   private static final Logger log = Logger.getLogger(FileServlet.class.getName());
-	   private String bucketName = "bw2project_data";
-	   /**Used below to determine the size of chucks to read in. Should be > 1kb and < 10MB */
-	   private static final int BUFFER_SIZE = 2 * 1024 * 1024;
-	   private final GcsService gcsService = GcsServiceFactory.createGcsService(new RetryParams.Builder()
-			    .initialRetryDelayMillis(10)
-			    .retryMaxAttempts(10)
-			    .totalRetryPeriodMillis(15000)
-			    .build());
-	
-	    @Override
-	    public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	    {
-	        String customer_name = request.getParameter("customer_name");
+   private static final Logger log = Logger.getLogger(FileServlet.class.getName());
+   private String bucketName = "bw2project_data";
+   /**Used below to determine the size of chucks to read in. Should be > 1kb and < 10MB */
+   private static final int BUFFER_SIZE = 2 * 1024 * 1024;
+   private final GcsService gcsService = GcsServiceFactory.createGcsService(new RetryParams.Builder()
+		    .initialRetryDelayMillis(10)
+		    .retryMaxAttempts(10)
+		    .totalRetryPeriodMillis(15000)
+		    .build());
 
-	        try 
-	        {
-	            GcsFilename gcsfileName = new GcsFilename(bucketName, customer_name);
-	            GcsInputChannel readChannel = gcsService.openPrefetchingReadChannel(gcsfileName, 0, BUFFER_SIZE);
-	        	
-	        }
-	        catch (Exception ex) 
-	        {
-	            throw new ServletException(ex);
-	        }
-	    }
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        String customer_name = request.getParameter("customer_name");
+
+        try 
+        {
+            GcsFilename gcsfileName = new GcsFilename(bucketName, customer_name);
+            GcsInputChannel readChannel = gcsService.openPrefetchingReadChannel(gcsfileName, 0, BUFFER_SIZE);
+        	
+        }
+        catch (Exception ex) 
+        {
+            throw new ServletException(ex);
+        }
+    }
 }
